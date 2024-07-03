85757c8d81301b94082bc4f01ac73ebb743506e2
Sadiki Benda <Sadiki.benda1@gmail.com>
2024-07-02 20:53:16 -0400

    card height updated

--- script.js
+++ script.js
@@ -5,8 +5,3 @@ share.addEventListener('click', () => {
     popup.style.display = 'flex';
 });
 
-if(window.innerWidth < 768) {
-    share.addEventListener('click', () => {
-        popup.style.display = 'flex';  // input for mobile
-    });
-}
